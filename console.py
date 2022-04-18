dda4f4031921f08598415f4322b6b1ebce1bc239
Uzaif Ahmad Shaikh <suzaifahmad@gmail.com>
2022-04-18 15:27:30 +0530

    update

--- console.py
+++ console.py
@@ -1,30 +1,49 @@
+# Importing Libraries
+import os,time
+os.system('cls')
 import pandas as pd
 import pyodbc
+# # # Connecting With Database
 conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=ZIL1180\MSSQLDEV2019;'
-                      'Database=inventory;'
-                      'Trusted_Connection=yes;')
+                    'Server=ZIL1180\MSSQLDEV2019;'
+                    'Database=inventory;'
+                    'Trusted_Connection=yes;')
+animation = "|/-\\"
+idx = 0
+while True:
+    print("Connecting With Database..",animation[idx % len(animation)], end="\r")
+    idx += 1
+    time.sleep(0.1)
+    if idx==30:
+        break
+os.system('cls')
+print("Connected Successfully")
 
+
+# # #Opening A Cursor
 cursor=conn.cursor()
 
-cursor.execute("SELECT * FROM products")
-products=[list(i) for i in cursor.fetchall()]
-df=pd.DataFrame(products,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
-print(df)
+#Fetching All Products from inventory
+def inventory():
+    
+    cursor.execute("SELECT * FROM products")
+    products=[list(i) for i in cursor.fetchall()]
+    df=pd.DataFrame(products,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
+    print(df)
 
 
-prod_id = input("Please Enter Product Id")
-cursor.execute('SELECT * FROM products where pid=?',prod_id)
-stock=[list(i) for i in cursor.fetchall()]
-stock_df=pd.DataFrame(stock,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
-print(stock_df)
-quantity = input("Please Enter Quantity: ")
-cursor.execute('UPDATE products SET avl_quant=avl_quant+? where pid=?',(quantity,prod_id))
-cursor.commit()
-cursor.execute('SELECT * FROM products where pid=?',prod_id)
-stock=[list(i) for i in cursor.fetchall()]
-stock_df=pd.DataFrame(stock,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
-print(stock_df)
+# # prod_id = input("Please Enter Product Id")
+# # cursor.execute('SELECT * FROM products where pid=?',prod_id)
+# # stock=[list(i) for i in cursor.fetchall()]
+# # stock_df=pd.DataFrame(stock,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
+# # print(stock_df)
+# # quantity = input("Please Enter Quantity: ")
+# # cursor.execute('UPDATE products SET avl_quant=avl_quant+? where pid=?',(quantity,prod_id))
+# # cursor.commit()
+# # cursor.execute('SELECT * FROM products where pid=?',prod_id)
+# # stock=[list(i) for i in cursor.fetchall()]
+# # stock_df=pd.DataFrame(stock,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
+# # print(stock_df)
 
 product_id = input("Please Enter Product Id:")
 cursor.execute('SELECT * FROM products where pid=?',product_id)
@@ -38,3 +57,12 @@ cursor.execute('SELECT * FROM products where pid=?',product_id)
 sale=[list(i) for i in cursor.fetchall()]
 sale=pd.DataFrame(sale[0])
 print(sale)
+
+def display():
+    print("1. Sales")
+    print("2. Inventory")
+    print("3. Out of Stock")
+    print("4. Orders")
+    print("5. Customers")
+
+display()
