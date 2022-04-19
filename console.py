392292281ecfd0dbb477f57c5f0dd583233fdd7a
Uzaif Ahmad Shaikh <suzaifahmad@gmail.com>
2022-04-19 16:33:51 +0530

    updateed

--- console.py
+++ console.py
@@ -8,36 +8,46 @@ conn = pyodbc.connect('Driver={SQL Server};'
                     'Server=ZIL1180\MSSQLDEV2019;'
                     'Database=inventory;'
                     'Trusted_Connection=yes;')
-animation = "|/-\\"
-idx = 0
-while True:
-    print("Connecting With Database..",animation[idx % len(animation)], end="\r")
-    idx += 1
-    time.sleep(0.1)
-    if idx==30:
-        break
-os.system('cls')
-print("Connected Successfully")
+# animation = "|/-\\"
+# idx = 0
+# while True:
+#     print("Connecting With Database..",animation[idx % len(animation)], end="\r")
+#     idx += 1
+#     time.sleep(0.1)
+#     if idx==30:
+#         break
+# os.system('cls')
+# print("Connected Successfully")
 
 
 # # #Opening A Cursor
 cursor=conn.cursor()
 
 #Fetching All Products from inventory
+cursor.execute("SELECT * FROM products")
+products=[list(i) for i in cursor.fetchall()]
 def inventory():
-    
     cursor.execute("SELECT * FROM products")
     products=[list(i) for i in cursor.fetchall()]
     df=pd.DataFrame(products,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
-    print(df)
+inventory()
 
-def sales(order):
+def calculate(productid,quantity):
+    # comment: to calculate total amount 
+    for i in products:
+        if i[0]==productid:
+            return (i[0],i[1],i[4]*quantity)
+# end def
+
+def sales(order,products):
     productid=int(input("Please Enter Product ID:"))
     quantity=int(input("Please Enter Quantity:"))
+    calculate(productid,quantity)
+
     order.append((productid,quantity))
     choice=input("Do You want to add more?? Y/N?")
     if choice=='y':
-        sales(order)
+        sales(order,products)
     else:
         print(order)
         for i in order:
@@ -45,7 +55,7 @@ def sales(order):
             cursor.commit()
         return
 order=[]
-sales(order)
+sales(order,products)
 
 # # prod_id = input("Please Enter Product Id")
 # # cursor.execute('SELECT * FROM products where pid=?',prod_id)
@@ -60,18 +70,18 @@ sales(order)
 # # stock_df=pd.DataFrame(stock,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
 # # print(stock_df)
 
-product_id = input("Please Enter Product Id:")
-cursor.execute('SELECT * FROM products where pid=?',product_id)
-sale=[list(i) for i in cursor.fetchall()]
-sale=pd.DataFrame(sale,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
-print(sale)
-quantity = input("Please Enter Quantity: ")
-cursor.execute('UPDATE products SET avl_quant=avl_quant-? where pid=?',(quantity,product_id))
-cursor.commit()
-cursor.execute('SELECT * FROM products where pid=?',product_id)
-sale=[list(i) for i in cursor.fetchall()]
-sale=pd.DataFrame(sale[0])
-print(sale)
+# product_id = input("Please Enter Product Id:")
+# cursor.execute('SELECT * FROM products where pid=?',product_id)
+# sale=[list(i) for i in cursor.fetchall()]
+# sale=pd.DataFrame(sale,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
+# print(sale)
+# quantity = input("Please Enter Quantity: ")
+# cursor.execute('UPDATE products SET avl_quant=avl_quant-? where pid=?',(quantity,product_id))
+# cursor.commit()
+# cursor.execute('SELECT * FROM products where pid=?',product_id)
+# sale=[list(i) for i in cursor.fetchall()]
+# sale=pd.DataFrame(sale[0])
+# print(sale)
 
 def display():
     print("1. Sales")
@@ -80,4 +90,4 @@ def display():
     print("4. Orders")
     print("5. Customers")
 
-display()
+# display()
