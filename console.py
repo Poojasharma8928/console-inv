f1c7552c15f3a9d24deb4f24219c57e314ac13b1
pooja sharma <poojasharma8928@gmail.com>
2022-04-20 12:07:10 +0530

    Chnages in console(Pooja)

--- console.py
+++ console.py
@@ -6,31 +6,32 @@ import pyodbc
 # # # Connecting With Database
 conn = pyodbc.connect('Driver={SQL Server};'
                     'Server=ZIL1157\MSSQLSRVDEV19;'
-                    'Database=Project_inventory;'
+                    'Database=Inventory;'
                     'Trusted_Connection=yes;')
-# animation = "|/-\\"
-# idx = 0
-# while True:
-#     print("Connecting With Database..",animation[idx % len(animation)], end="\r")
-#     idx += 1
-#     time.sleep(0.1)
-#     if idx==30:
-#         break
-# os.system('cls')
-# print("Connected Successfully")
+animation = "|/-\\"
+idx = 0
+while True:
+    print("Connecting With Database..",animation[idx % len(animation)], end="\r")
+    idx += 1
+    time.sleep(0.1)
+    if idx==30:
+        break
+os.system('cls')
+print("Connected Successfully")
 
 
 # # #Opening A Cursor
 cursor=conn.cursor()
 
 #Fetching All Products from inventory
-cursor.execute("SELECT * FROM products")
+cursor.execute("SELECT * FROM Product")
 products=[list(i) for i in cursor.fetchall()]
 def inventory():
-    
     cursor.execute("SELECT * FROM Product")
     products=[list(i) for i in cursor.fetchall()]
-    df=pd.DataFrame(products,columns=["Product_Id","Product_Name","Category","Available Quantity","Price"])
+    df=pd.DataFrame(products,columns=["Product_Id","Product_Name","Category","Available Quantity","Price","Reorder Level"])
+    print(df)
+    
 inventory()
 
 def calculate(productid,quantity):
@@ -39,21 +40,14 @@ def calculate(productid,quantity):
         if i[0]==productid:
             return (i[0],i[1],i[4]*quantity)
 # end def
-
-def sales(order,products):
-    productid=int(input("Please Enter Product ID:"))
-    quantity=int(input("Please Enter Quantity:"))
-    calculate(productid,quantity)
-
-    order.append((productid,quantity))
-    choice=input("Do You want to add more?? Y/N?")
+    
 def sales(order):
     product_id=int(input("Please Enter Product ID:"))
     quantity=int(input("Please Enter Quantity:"))
     order.append((product_id,quantity))
     choice=input("Do You want to Purchase more?? Y/N?")
     if choice=='y':
-        sales(order,products)
+        sales(order)
     else:
         print(order)
         for i in order:
@@ -61,14 +55,14 @@ def sales(order):
             cursor.commit()
         return
 order=[]
-sales(order,products)
+sales(order)
 
 
-# def display():
-#     print("1. Sales")
-#     print("2. Inventory")
-#     print("3. Out of Stock")
-#     print("4. Orders")
-#     print("5. Customers")
+def display():
+    print("1. Sales")
+    print("2. Inventory")
+    print("3. Out of Stock")
+    print("4. Orders")
+    print("5. Customers")
 
-# display()
+display()
